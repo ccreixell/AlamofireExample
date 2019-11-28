5546057f3f8eaaf4776f7f15827466af77c1a0aa
ccreixel <cristina.creixell-amigo@capgemini.com>
2019-11-28 11:58:43 +0100

    Initial Commit

---
+++ AlamofireExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireExample
+//
+//  Created by capgemini on 28/11/2019.
+//  Copyright © 2019 Creixell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
